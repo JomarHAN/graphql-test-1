c29bec788720927ac4c140e096ed3c86e2edc8c1
Jomar HA Nguyen <jomarhan.ng@gmail.com>
2021-06-20 18:43:41 -0400

    done test BE graphql

--- server/server.js
+++ server/server.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const app = express()
 const PORT = 6969
-const userData = require('./MOCK_DATA.json')
+
+const { graphqlHTTP } = require('express-graphql')
+
+const schema = require('./Schemas')
+
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: true
+}))
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`)
